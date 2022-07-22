11d8d97a2bf38c0bc2f5fc9e42e553b8804eed79
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-07-22 21:52:06 +1000

    cards are added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React, { Component } from 'react';
+
 import styles from './App.module.css';
 import Title from './Title/Title';
 import Background from './Background/Background';
@@ -5,16 +7,29 @@ import Grid from './Grid/Grid';
 import Rocket from './Rocket/Rocket';
 import Smoke from './Rocket/Smoke';
 
-function App() {
-  return (
-    <div className={styles.App}>
-      <Title />
-      <Background />
-      <Grid />
-      <Rocket />
-      <Smoke />
-    </div>
-  );
+// data
+import facts from './data/NASA_facts.json';
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      facts,
+    }
+  }
+  render() {
+    return (
+      <div className={styles.App}>
+        <Title />
+        <Background />
+        <Grid facts={this.state.facts}/>
+        <Rocket />
+        <Smoke />
+      </div>
+    );
+  }
+
 }
 
 export default App;
--- src/Background/Background.js
+++ src/Background/Background.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from '.Background.module.css';
+import styles from './Background.module.css';
 
 export default class App extends React.Component {
     render() {
