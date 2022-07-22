ee0385496057dcaf198c55bf75614b2f76462019
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-07-22 19:32:16 +1000

    adding component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import styles from './App.module.css';
+import Title from './Title/Title';
+import Background from './Background/Background';
+import Grid from './Grid/Grid';
+import Rocket from './Rocket/Rocket';
+import Smoke from './Rocket/Smoke';
 
 function App() {
   return (
     <div className={styles.App}>
-
+      <Title />
+      <Background />
+      <Grid />
+      <Rocket />
+      <Smoke />
     </div>
   );
 }
